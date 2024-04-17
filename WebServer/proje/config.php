3e6d86df224cb7f5682778f78e8ee53e61ee024e
Mikail AKSU <95864086+mikailaksu@users.noreply.github.com>
2024-04-17 15:48:53 +0300

    Create config.php

---
+++ WebServer/proje/config.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'nfceye';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
